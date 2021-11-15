d39ad99c9e197bb07084d6ea34f226f86ecb2c98
cwher <cwher@outlook.com>
2021-11-15 15:22:46 +0800

    to: upd network & add tests

--- little-w/agent/network.py
+++ little-w/agent/network.py
@@ -1,5 +1,3 @@
-from collections import deque
-
 import numpy as np
 import torch
 import torch.nn as nn
@@ -8,92 +6,7 @@ import torch.optim as optim
 from config import NETWORK_CONFIG, TRAIN_CONFIG
 from icecream import ic
 
-# TODO
-# HACK: padding pattern in Con2D need changes
-
-
-class ObsEncoder():
-    def __init__(self) -> None:
-        self.states = deque(
-            maxlen=NETWORK_CONFIG.periods_num)
-
-    def add(self, raw_state):
-        """[summary]
-
-        Args:
-            raw_state ([type]): [description] output of env
-        """
-        raw_state = raw_state[0]
-        state = [raw_state[i] for i in range(1, 8)]
-        while (len(self.states) <
-               NETWORK_CONFIG.periods_num):
-            self.states.append(state)
-
-    def encode(self, turn) -> np.ndarray:
-        """[summary]
-        features selection
-
-        """
-        bean_index = 0
-        my_indices = [1, 2, 3] if turn == 0 else [4, 5, 6]
-        opponent_indices = [4, 5, 6] if turn == 0 else [1, 2, 3]
-
-        periods_num = NETWORK_CONFIG.periods_num
-        features = np.zeros(
-            (periods_num * 2 + 4, 10, 20))
-
-        # positions of my body in previous k periods
-        #   (including current period)
-        for i in range(periods_num):
-            for j in my_indices:
-                features[i][tuple(zip(*self.states[i][j]))] = 1
-        # current positions of my head
-        for i in my_indices:
-            x, y = self.states[-1][i][0]
-            features[periods_num][x][y] = 1
-
-        # positions of opponent's body in previous k periods
-        #   (including current period)
-        for i in range(periods_num):
-            for j in opponent_indices:
-                features[periods_num + 1 +
-                         i][tuple(zip(*self.states[i][j]))] = 1
-        # current positions of opponent's head
-        for i in opponent_indices:
-            x, y = self.states[-1][i][0]
-            features[periods_num * 2 + 1][x][y] = 1
-
-        # positions of all the beans
-        features[-2][tuple(zip(*self.states[-1][bean_index]))] = 1
-
-        # all 0 if turn == 0 else all 1
-        if turn == 1:
-            features[-1, ...] = np.ones_like(features[-1, ...])
-
-        return features
-
-
-def conv3x3(in_channels, out_channels):
-    return nn.Conv2d(
-        in_channels, out_channels,
-        kernel_size=3, padding=1,
-        padding_mode="circular", bias=False)
-
-
-class ResBlock(nn.Module):
-    def __init__(self, num_channels):
-        super().__init__()
-
-        self.conv1 = conv3x3(num_channels, num_channels)
-        self.bn1 = nn.BatchNorm2d(num_channels)
-
-        self.conv2 = conv3x3(num_channels, num_channels)
-        self.bn2 = nn.BatchNorm2d(num_channels)
-
-    def forward(self, x):
-        y = F.relu(self.bn1(self.conv1(x)))
-        y = self.bn2(self.conv2(y))
-        return F.relu(x + y)
+from .network_utils import ResBlock, conv3x3
 
 
 class Network(nn.Module):
@@ -148,13 +61,15 @@ class PolicyValueNet():
             lr=TRAIN_CONFIG.learning_rate,
             weight_decay=TRAIN_CONFIG.l2_weight)
 
-    def save(self):
-        pass
-        # TODO
+    def save(self, version="w"):
+        torch.save(
+            self.net.state_dict(),
+            TRAIN_CONFIG.checkpoint_dir +
+            "/model_" + version)
 
     def load(self, model_dir):
-        pass
-        # TODO
+        self.net.load_state_dict(torch.load(
+            model_dir, map_location=torch.device("cuda:0")))
 
     def predict(self, features):
         """[summary]
--- little-w/config.py
+++ little-w/config.py
@@ -1,8 +1,19 @@
 from collections import namedtuple
 
+# NOTE: dictionaries are transformed to namedtuples
+
+MDP_CONFIG = {
+    "xxx": 1,
+    "final_reward": 10,
+}
+
 
 NETWORK_CONFIG = {
-
+    "periods_num": 5,
+    "num_channels": 256,
+    "num_res": 4,
+    "board_size": 10 * 20,
+    "action_size": 64,
 }
 
 MCTS_CONFIG = {
@@ -14,5 +25,23 @@ DATA_GEN_CONFIG = {
 }
 
 TRAIN_CONFIG = {
+    "l2_weight": 1e-4,
+    "learning_rate": 0.001,
+    "check_point": "check_point"
 
 }
+
+MDP_CONFIG_TYPE = namedtuple("MDP_CONFIG", MDP_CONFIG.keys())
+MDP_CONFIG = MDP_CONFIG_TYPE._make(MDP_CONFIG.values())
+
+NETWORK_CONFIG_TYPE = namedtuple("NETWORK_CONFIG", NETWORK_CONFIG.keys())
+NETWORK_CONFIG = NETWORK_CONFIG_TYPE._make(NETWORK_CONFIG.values())
+
+MCTS_CONFIG_TYPE = namedtuple("MCTS_CONFIG", MCTS_CONFIG.keys())
+MCTS_CONFIG = MCTS_CONFIG_TYPE._make(MCTS_CONFIG.values())
+
+DATA_GEN_CONFIG_TYPE = namedtuple("DATA_GEN_CONFIG", DATA_GEN_CONFIG.keys())
+DATA_GEN_CONFIG = DATA_GEN_CONFIG_TYPE._make(DATA_GEN_CONFIG.values())
+
+TRAIN_CONFIG_TYPE = namedtuple("TRAIN_CONFIG", TRAIN_CONFIG.keys())
+TRAIN_CONFIG = TRAIN_CONFIG_TYPE._make(TRAIN_CONFIG.values())
