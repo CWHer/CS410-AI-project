e4994c2118bb9cb3d8ea08437c0c6eb0c312a9f1
cwher <cwher@outlook.com>
2021-11-19 15:32:45 +0800

    tests: upd game, buffer test

--- tests/test_game.py
+++ tests/test_game.py
@@ -3,6 +3,7 @@ from agent.network import PolicyValueNet
 from train_utils.game import contest, selfPlay
 from train_utils.replay_buffer import ReplayBuffer
 
+from tqdm import tqdm
 
 net = PolicyValueNet()
 states, mcts_probs, values = selfPlay(net, seed=1)
@@ -12,10 +13,15 @@ replay_buf = ReplayBuffer()
 replay_buf.add(states, mcts_probs, values)
 replay_buf.save(version="test")
 
-# test training
-# train_iter = replay_buf.trainIter()
-# for data_batch in train_iter:
-#     net.trainStep(data_batch)
-
 winner = contest(net, net, seed=1)
 ic(winner)
+
+# test training
+ic(replay_buf.size())
+train_iter, iter_len = replay_buf.trainIter()
+ic(iter_len)
+with tqdm(total=iter_len) as pbar:
+    for data_batch in train_iter:
+        loss, acc = net.trainStep(data_batch)
+        ic(loss, acc)
+        pbar.update()
