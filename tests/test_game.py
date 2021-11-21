0f6678450ad5adb40c709e6b5c5089bcc60034d2
cwher <cwher@outlook.com>
2021-11-21 21:45:37 +0800

    tests: upd train_iter test

--- tests/test_game.py
+++ tests/test_game.py
@@ -1,11 +1,15 @@
+from itertools import chain
+
+import torch
 from icecream import ic
+from tqdm import tqdm
+
 from agent.network import PolicyValueNet
 from train_utils.game import contest, selfPlay
 from train_utils.replay_buffer import ReplayBuffer
 
-from tqdm import tqdm
-
 net = PolicyValueNet()
+# net.setDevice(torch.device("cpu"))
 states, mcts_probs, values = selfPlay(net, seed=1)
 
 # construct dataset
@@ -18,10 +22,11 @@ ic(winner)
 
 # test training
 ic(replay_buf.size())
-train_iter, iter_len = replay_buf.trainIter()
-ic(iter_len)
-with tqdm(total=iter_len) as pbar:
-    for data_batch in train_iter:
-        loss, acc = net.trainStep(data_batch)
-        ic(loss, acc)
-        pbar.update()
+train_iter = replay_buf.trainIter()
+iter_len = len(train_iter[0]) + len(train_iter[1])
+for i in range(2):
+    with tqdm(total=iter_len) as pbar:
+        for data_batch in chain(*train_iter):
+            loss, acc = net.trainStep(data_batch)
+            ic(loss, acc)
+            pbar.update()
