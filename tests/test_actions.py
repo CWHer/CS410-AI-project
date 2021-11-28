a70e075a43efcd03e2f293345768fa168fe1587d
cwher <cwher@outlook.com>
2021-11-28 14:28:59 +0800

    test: add basic network, action, original env tests

---
+++ tests/test_actions.py
@@ -0,0 +1,14 @@
+from icecream import ic
+
+from env.utils import Actions, ActionsFilter
+
+ic(Actions.inv(Actions.UP))
+ic(ActionsFilter.Act2Idx([Actions.RIGHT, Actions.RIGHT, Actions.RIGHT]))
+ic(ActionsFilter.Idx2Arr(63))
+
+last_actions = ActionsFilter.extractActions(
+    [[[0, 1], [0, 2]], [[0, 1], [1, 1]], [[0, 1], [8, 1]]])
+ic(last_actions)
+indices = ActionsFilter.genActions(last_actions)
+for index in indices:
+    ic(ActionsFilter.Idx2Act(index))
