8687398682244177c457d7beec9766867fa27dce
cwher <cwher@outlook.com>
2021-11-28 19:51:42 +0800

    feat: add game & replay_buffer

---
+++ D3QN/train_utils/game.py
@@ -0,0 +1,103 @@
+import random
+
+import numpy as np
+import torch
+from env.simulator import Simulator
+from icecream import ic
+from utils import printError
+
+
+def epsilonGreedy(q_values, indices, epsilon=0.1):
+    ic.configureOutput(includeContext=True)
+    printError(
+        q_values.shape[-1] != len(indices),
+        ic.format("shapes do not match!"))
+    ic.configureOutput(includeContext=False)
+
+    if np.random.rand() < epsilon:
+        return random.choice(indices)
+    else:
+        return indices[q_values.argmax(axis=1).item()]
+
+
+def selfPlay(net, epsilon, seed):
+    """[summary]
+    self play and gather experiences
+
+    Args:
+        net ([type]): [description]
+
+    Returns:
+        states [List[np.ndarray]]: [description]
+        mcts_probs [List[np.ndarray]]: [description]
+        values [List[float]]: [description]
+    """
+    # initialize seeds (torch, np, random)
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+
+    data_buffer = []
+    env = Simulator()
+    state = env.reset()
+
+    while True:
+        # NOTE: last_actions is used in enhanceData
+        prev_actions = env.encoder.getLastActions()
+        indices = env.validActions()
+        actions = [None] * 2
+        for i in range(2):
+            q_values = net.predict(state[i])
+            actions[i] = epsilonGreedy(q_values, indices[i], epsilon)
+
+        next_state, reward, done, _ = env.step(actions)
+
+        # collect data
+        data_buffer.append(
+            (state[0], reward, actions[0],
+             prev_actions[:3], next_state[0], done))
+        data_buffer.append(
+            (state[1], reward, actions[1],
+             prev_actions[3:], next_state[1], done))
+
+        state = next_state
+        # debug
+        # env.drawBoard()
+
+        if done:
+            env.score_board.showResults()
+            return list(zip(*data_buffer))
+
+
+def contest(net0, net1, seed):
+    """[summary]
+    contest between net0 and net1
+
+    Returns:
+        int: [description]. winner
+    """
+    # initialize seeds (torch, np, random)
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+
+    env = Simulator()
+    state = env.reset()
+    players = [net0, net1]
+
+    while True:
+        indices = env.validActions()
+        actions = [None] * 2
+        for i in range(2):
+            q_values = players[i].predict(state[i])
+            actions[i] = epsilonGreedy(
+                q_values, indices[i], epsilon=0)
+
+        next_state, reward, done, _ = env.step(actions)
+
+        # debug
+        # env.drawBoard()
+
+        if done:
+            env.score_board.showResults()
+            return env.score_board.getWinner()
---
+++ D3QN/train_utils/replay_buffer.py
@@ -0,0 +1,126 @@
+import itertools
+import pickle
+from collections import deque, namedtuple
+from itertools import product
+
+import numpy as np
+import torch
+from env.utils import ActionsFilter, Actions
+from config import MDP_CONFIG, TRAIN_CONFIG
+from icecream import ic
+from torch.utils.data import DataLoader, TensorDataset
+from utils import plotHeatMaps, plotSparseMatrix
+
+
+class ReplayBuffer():
+    Data = namedtuple("data", "state reward action next_state done")
+
+    def __init__(self) -> None:
+        # NOTE: separate 10x20 and 20x10
+        self.buffer = [
+            deque(maxlen=TRAIN_CONFIG.replay_size)
+            for _ in range(2)]
+
+    def size(self):
+        return len(self.buffer[0]) + len(self.buffer[1])
+
+    def save(self, version="w"):
+        dataset_dir = TRAIN_CONFIG.dataset_dir
+
+        import os
+        if not os.path.exists(dataset_dir):
+            os.mkdir(dataset_dir)
+
+        print("save replay buffer version({})".format(version))
+        with open(dataset_dir +
+                  f"/data_{version}.pkl", "wb") as f:
+            pickle.dump(self.buffer, f)
+
+    def load(self, data_dir):
+        print("load replay buffer {}".format(data_dir))
+        with open(data_dir, "rb") as f:
+            self.buffer = pickle.load(f)
+
+    def enough(self):
+        """[summary]
+        whether data is enough to start training
+        """
+        return self.size() > TRAIN_CONFIG.train_threshold
+
+    def __enhanceData(self,
+                      states, rewards, actions,
+                      prev_actions, next_states, dones):
+        """[summary]
+        enhance data by rotating and flipping
+        """
+        enhanced_data = [[], []]
+        for state, reward, action, prev_action, next_state, done in zip(
+                states, rewards, actions, prev_actions, next_states, dones):
+            # rotating and flipping
+            for i in range(4):
+                # rotate
+                new_state = np.rot90(state, i, axes=(1, 2))
+                new_next_state = np.rot90(next_state, i, axes=(1, 2))
+                new_action = ActionsFilter.rot90(action, i)
+                new_prev_action = list(
+                    map(lambda x: Actions.rot90(x, i), prev_action))
+                new_actions = ActionsFilter.genActions(new_prev_action)
+                # NOTE: calculate relative index
+                enhanced_data[i & 1].append(
+                    (new_state, reward,
+                     new_actions.index(new_action), new_next_state, done))
+
+                # debug
+                # ic(new_prev_action)
+                # ic(new_action)
+                # print(ActionsFilter.Idx2Act(new_action))
+                # print(new_actions.index(new_action))
+                # plotSparseMatrix(new_state[2], "none")
+                # plotSparseMatrix(new_next_state[2], "none")
+
+                # flip
+                new_state = np.array([np.fliplr(s) for s in new_state])
+                new_next_state = np.array([
+                    np.fliplr(s) for s in new_next_state])
+                new_action = ActionsFilter.fliplr(new_action)
+                new_prev_action = list(
+                    map(lambda x: Actions.fliplr(x), new_prev_action))
+                new_actions = ActionsFilter.genActions(new_prev_action)
+                # NOTE: calculate relative index
+                enhanced_data[i & 1].append(
+                    (new_state, reward,
+                     new_actions.index(new_action), new_next_state, done))
+
+                # debug
+                # ic(new_prev_action)
+                # ic(new_action)
+                # print(ActionsFilter.Idx2Act(new_action))
+                # print(new_actions.index(new_action))
+                # plotSparseMatrix(new_state[2], "none")
+                # plotSparseMatrix(new_next_state[2], "none")
+
+        # debug
+        # for i in range(4):
+        #     plotSparseMatrix(enhanced_data[0][i][0][2], "none")
+        return enhanced_data
+
+    def add(self, episode_data):
+        enhanced_data = self.__enhanceData(*episode_data)
+        for i in range(2):
+            self.buffer[i].extend(enhanced_data[i])
+
+    def trainIter(self):
+        """[summary]
+        generate dataset iterator for training
+        NOTE: return 2 iterators that are from different buffers
+        """
+        train_iter = [None] * 2
+        for i in range(2):
+            # states, rewards, actions, next_states, dones
+            data_set = list(map(
+                lambda x: torch.from_numpy(np.stack(x)),
+                zip(*self.buffer[i])))
+            data_set = TensorDataset(*data_set)
+            train_iter[i] = DataLoader(
+                data_set, TRAIN_CONFIG.batch_size, shuffle=True)
+        return train_iter
