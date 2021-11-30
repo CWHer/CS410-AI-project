eff180d72d8aa83246941f2b7127590637738792
cwher <cwher@outlook.com>
2021-11-30 19:03:40 +0800

    to: upd config

--- D3QN/config.py
+++ D3QN/config.py
@@ -9,8 +9,8 @@ MDP_CONFIG = {
     "board_width": 20,
     "board_size": 10 * 20,
     "action_size": 27,
-    "c_reward": 1,
-    "final_reward": 50,
+    "c_reward": 0.2,
+    "final_reward": 10,
     "gamma": 0.98,
     "total_step": 200,
 }
@@ -18,37 +18,38 @@ MDP_CONFIG = {
 NETWORK_CONFIG = {
     "periods_num": 5,
     "in_channels":  None,
-    "num_channels": 256,
-    "num_res": 4,
+    "num_channels": 128,
+    "num_res": 2,
     "update_freq": 100,
 }
 NETWORK_CONFIG["in_channels"] = \
     NETWORK_CONFIG["periods_num"] * 2 + 6
 
 TRAIN_CONFIG = {
-    "train_num": 1000000,
-    "train_epochs": 2,
-    "learning_rate": 1e-5,
+    "train_num": 100000,
+    "train_epochs": 2000,
+    # "train_epochs": 5,
+    "learning_rate": 1e-4,
     "checkpoint_dir": "checkpoint",
-    "batch_size": 512,
+    "batch_size": 256,
     "train_threshold": 10000,
     "replay_size": 1000000,
     "dataset_dir": "dataset",
-    "data_save_freq": 20,
+    "data_save_freq": 500,
     "para_dir": "parameters",
 
-    "process_num": 4,
+    "process_num": 5,
     # evaluate model
-    "check_freq": 100,
+    "check_freq": 10,
     "update_threshold": 0.55,
     "num_contest": 20,
     # data generation
-    "game_num": 4,
+    "game_num": 10,
 
     # epsilon-greedy
     "init_epsilon": 0.8,
-    "min_epsilon": 0.05,
-    "delta_epsilon": 0.00005
+    "min_epsilon": 0.1,
+    "delta_epsilon": 0.01
 }
 
 
